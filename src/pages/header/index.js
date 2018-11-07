b064fb7016823d0ba3668e3dd215c35aa5e89cf1
yangyi <yangyi@cnabcpm.com>
2018-11-07 10:25:08 +0800

    successtogetinformation

--- src/pages/header/index.js
+++ src/pages/header/index.js
@@ -1,29 +1,30 @@
 import React, { Component } from 'react';
 import './index.less';
 import '../../static/style.css';
-import { connect } from 'dva';
+import { connect  } from 'dva';
 
-class Headers extends React.Component {
+class Header extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {CityName:''};
     }
 
     componentDidMount() {
         const { dispatch } = this.props;
         dispatch({
             type:"setting/getUserInfor",
-            payload: {//可以不填
+            payload: {
                 id:123,
               }
         })
     }
 
     render() {
-        console.log(this.props)
+        const {data}=this.props;
         return (
             <div id="home-header" className="clear-fix">
                 <div className="home-header-left float-left">
-                    <span>深圳</span>&nbsp;
+                    <span>{data?data.CityName:''}</span>&nbsp;
                 <i className="icon-chevron-down"></i>
                 </div>
                 <div className="home-header-right float-right">
@@ -40,18 +41,16 @@ class Headers extends React.Component {
     }
 }
 
-const mapStateToProps = (state,ownProps) => {  //见名知意，把state转换为props
+const mapStateToProps = (state) => { 
+    //见名知意，把state转换为props
     //可以打印state看看数据结构，然后放到data里
-    console.log(state)
     return {
-      data:state.data,
-    //   active: ownProps.filter === state.visibilityFilter,
-    //   todos: getVisibleTodos(state.todos, state.visibilityFilter)
+      data:state.setting.data.data,
     };
   };
 
-const Header= connect(mapStateToProps)(Headers);
-  export default Header;
+
+  export default connect(mapStateToProps)(Header);
 
 /*dispatch 是一个函数方法，用来将 Action 发送给 Model 中的State。
 dispatch方法从哪里来？被 connect 的 Component 会自动在 Props 中拥有 dispatch 方法。*/
