53eb8524476e78cbdc8d3a907bfaf28277d47ed2
yangyi <yangyi@cnabcpm.com>
2018-11-08 19:41:17 +0800

    npm axious

--- src/pages/components/List/list.js
+++ src/pages/components/List/list.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import '../header.less'
 import './list.less';
+import cookie from 'react-cookie';
 
+if (process.env.NODE_ENV === 'development') {
+    require('../../../../mock/todoList')
+    }
+    let token = cookie.load('token');
+    let postApi = (path, mock) => {
+    return path + (mock ? '' : '.mock') + '?token=' + token;
+    };
+    const mockTodo={
+        "newsList":postApi("/api/order/news"),
+        "newsList2":postApi("/api/newsList2"),
+        "todoList":postApi("/todoList", 0)
+    }
 export default class ExhibitionList extends React.Component {
+
+ 
 
     render() {
         return (
             <div className="list-item clear-fix">
                 <div className="item-img-container float-left">
-                    <img />
+                    <img />{mockTodo}
                 </div>
                 <div className="item-content">
                     <div className="item-title-container">
