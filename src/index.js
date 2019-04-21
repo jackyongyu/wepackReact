136ae15a49bc64dbd827452cbb235a0c87e24378
yangyi <m17710252505@163.com>
2019-04-21 14:24:12 +0800

    19421仿大众点评

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import dva from 'dva';
 import Header from './components/js/index';
-// 创建应用
-const app = dva();
-// 注册视图
-app.router(() => <Header />);
-// 启动应用
-app.start('#root');
+
 
 
 
